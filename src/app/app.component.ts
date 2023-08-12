13d6fcd60c0ddc139b14148b18d6f8738609e465
Andreson Souza <andresonjazz@gmail.com>
2023-08-12 19:17:27 -0300

    configure a exibição do header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'mt-app',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'meat-app';
+
+  content = 'Welcome do Meat App!'
+
 }
