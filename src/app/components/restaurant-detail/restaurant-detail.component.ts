c62a41851c12a22bcb5c446cc4c24c65d04a3c3f
Andreson Souza <andresonjazz@gmail.com>
2024-02-08 19:41:38 -0300

    atualização de código

--- src/app/components/restaurant-detail/restaurant-detail.component.ts
+++ src/app/components/restaurant-detail/restaurant-detail.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-restaurant-detail',
-  standalone: true,
-  imports: [],
   templateUrl: './restaurant-detail.component.html',
-  styleUrl: './restaurant-detail.component.css'
+  styleUrls: ['./restaurant-detail.component.css']
 })
 export class RestaurantDetailComponent {
 
