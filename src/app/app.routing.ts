4746eab56b5ebde737c1a816f4f6ea7f25e0d8ee
Andreson Oliveira de Souza <asouza@debian.intelbras.local>
2024-02-15 22:06:16 -0300

    adicione o layout da tela de login

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { RestaurantsComponent } from './components/restaurants/restaurants.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
+  // { path: '', component: LoginComponent },
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'restaurants', component: RestaurantsComponent }
